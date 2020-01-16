cda0319e0d1246a36bbd8c8c5d7f4f2d49cf4b3a
Pavel-Novoselov <npe@inbox.ru>
2020-01-17 00:06:46 +0300

    HomeWork lesson5

--- src/lesson5/hw/Main2.java
+++ src/lesson5/hw/Main2.java
@@ -1,4 +1,44 @@
 package lesson5.hw;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class Main2 {
+
+    public static void main(String[] args) {
+//спосок вещей (имя/вес/цена)
+        ArrayList<Things> list=new ArrayList<>();
+        Things thing1 = new Things("thing1", 1f, 100);
+        Things thing2 = new Things("thing2", 1.1f, 160);
+        Things thing3 = new Things("thing3", 0.5f, 60);
+        Things thing4 = new Things("thing4", 0.7f, 90);
+        Things thing5 = new Things("thing5", 1.4f, 130);
+        list.add(thing1);
+        list.add(thing2);
+        list.add(thing3);
+        list.add(thing4);
+        list.add(thing5);
+
+ //сортируем вещи по удельной стоимости
+        list.sort(new Comparator<Things>() {
+            @Override
+            public int compare(Things o1, Things o2) {
+                return (int)(o2.udelPrice-o1.udelPrice);
+            }
+        });
+//создаем рюкзак и указ. его макс вес
+        Ruckzack ruckzack = new Ruckzack(2.9f);
+//добавляем в рюкзак вначале вещи с самой большой удельной стоимостью, пока есть место в рюкзаке
+        for (Things t: list) {
+            if (ruckzack.getCurrent_weight()+t.getWeight()>ruckzack.getMax_waight())
+                break;
+            ruckzack.addThing(t);
+        }
+
+        System.out.println("Итого стомость рюкзака- "+ruckzack.getCurrent_price());
+        System.out.println("Итого вес рюкзака- "+ruckzack.getCurrent_weight());
+        System.out.println("Что положили- "+ruckzack.getThings());
+
+    }
+
 }
--- src/lesson5/hw/Things.java
+++ src/lesson5/hw/Things.java
@@ -1,4 +1,52 @@
 package lesson5.hw;
 
 public class Things {
+    String name;
+    float weight;
+    int price;
+    float udelPrice; //удельная стоимость вещи
+
+    public Things(String name, float weight, int price) {
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+        udelPrice = this.price/this.weight;
+    }
+
+    public float getUdelPrice() {
+        return udelPrice;
+    }
+
+    public void setUdelPrice(float udelPrice) {
+        this.udelPrice = udelPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return ("\n name=" + name+" weight="+weight+" price="+price+" udPrice="+udelPrice);
+    }
 }
